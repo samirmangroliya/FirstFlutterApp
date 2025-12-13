0a5005d65c581d7d34f71eba3cc358162c31d7db
Samir Mangroliya <samirmangroliya@users.noreply.github.com>
2025-12-13 23:41:52 +0530

    Merge pull request #3
    * Variables and Dynamic In Flutter
    
    * Variables and Dynamic In Flutter

---
+++ lib/variables/VarDynamicUse.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  //var
+  var subject = "Physics";
+  subject = "Maths";
+  print(subject);
+  //Dynamic
+  dynamic classNo; //you can also use var instead of dynamic
+
+  classNo = "A";
+  print(classNo);
+  classNo = 10;
+  print(classNo);
+  classNo = "10A";
+  print(classNo);
+  classNo = false;
+  print(classNo);
+}
